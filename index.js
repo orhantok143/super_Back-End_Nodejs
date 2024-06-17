8a8198f6a3e74ab859227945a823be13a1ee98a5
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-06-18 01:36:58 +0300

    v2.0.0

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Routers } from "./routers/indexRouter.js"
 import connectdDB from "./database/dbConnection.js"
 import errorHandler from "./middlewares/errorHandler.js"
 import morgan from "morgan"
-// import cors from "cors"
+import cors from "cors"
 dotenv.config()
 
 
@@ -14,11 +14,6 @@ const PORT = process.env.PORT || 5000
 // Database Connection
 connectdDB()
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // veya "*"
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 
 
@@ -36,7 +31,7 @@ app.use((req, res, next) => {
 // };
 
 // Middlewares
-// app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json({ limit: "100mb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev"))
