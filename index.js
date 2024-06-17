0b23c1914ce3583b54d30d4f0dffc98e7c213396
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-06-18 01:32:38 +0300

    v2.0.5

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express"
 import dotenv from "dotenv"
 import { Routers } from "./routers/indexRouter.js"
 import connectdDB from "./database/dbConnection.js"
-import errorHandler, { createError } from "./middlewares/errorHandler.js"
+import errorHandler from "./middlewares/errorHandler.js"
 import morgan from "morgan"
-import cors from "cors"
+// import cors from "cors"
 dotenv.config()
 
 
@@ -14,22 +14,29 @@ const PORT = process.env.PORT || 5000
 // Database Connection
 connectdDB()
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // veya "*"
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-const whitelist = ['http://localhost:3000', 'http://192.168.16.105:3000', "https://cafe-life.netlify.app"]; // İzin verilen kök URL'lerin listesi
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new createError(500, 'Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-};
+
+// const whitelist = ['http://localhost:3000', 'http://192.168.16.105:3000', "https://cafe-life.netlify.app"]; // İzin verilen kök URL'lerin listesi
+
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1 || !origin) {
+//             callback(null, true);
+//         } else {
+//             callback(new createError(500, 'Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true,
+// };
 
 // Middlewares
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 app.use(express.json({ limit: "100mb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev"))
