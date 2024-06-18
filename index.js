76e8261253cabd47ad32f50aae13eb93bf558b15
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-06-18 03:41:34 +0300

    v2.0.0

--- index.js
+++ index.js
@@ -17,21 +17,21 @@ connectdDB()
 
 
 
-// const whitelist = ['http://localhost:3000', 'http://192.168.16.105:3000', "https://cafe-life.netlify.app"]; // İzin verilen kök URL'lerin listesi
+const whitelist = ['http://localhost:3000']; // İzin verilen kök URL'lerin listesi
 
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//         if (whitelist.indexOf(origin) !== -1 || !origin) {
-//             callback(null, true);
-//         } else {
-//             callback(new createError(500, 'Not allowed by CORS'));
-//         }
-//     },
-//     credentials: true,
-// };
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new createError(500, 'Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+};
 
 // Middlewares
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json({ limit: "100mb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev"))
